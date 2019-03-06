978aeb3f56169b40ed345b78ba0c67751659ca1d
Amiel Cox <amielcox@gmail.com>
2019-03-05 18:48:00 -0800

    Check-In 1
    Check-In 1

---
+++ Assets/Scripts/EnemyCarMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCarMove : MonoBehaviour
+{
+    Transform playerObject;
+    UnityEngine.AI.NavMeshAgent navmesh;
+
+
+    void Awake()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player").transform;
+        navmesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        navmesh.SetDestination(playerObject.position);
+    }
+}
---
+++ Assets/Scripts/PoliceCarSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceCarSystem : MonoBehaviour
+{
+
+    public GameObject enemyObject;
+    public float spawnTime = 2f;
+    public Transform[] policeSpawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, policeSpawnPoints.Length);
+
+        Instantiate(enemyObject, policeSpawnPoints[spawnPointIndex].position, policeSpawnPoints[spawnPointIndex].rotation);
+    }
+}
